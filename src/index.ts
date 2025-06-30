c4d1e6bd430faaf9e5f7e7e8942a983018b580b4
Victor Leman <victorleman1@gmail.com>
2025-06-30 11:29:11 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("API artisan app is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI!)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+  })
+  .catch((err) => console.error("MongoDB connection error:", err));
